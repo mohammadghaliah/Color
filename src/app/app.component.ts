1f966aee4eee5ea775e134da7210598f8748eaf6
mohammadghaliah <94458537+mohammadghaliah@users.noreply.github.com>
2022-09-03 22:35:08 +0300

    5 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component, VERSION } from '@angular/core';
+import { Theme } from './core/utilities/theam';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  color() {
+    const color = {};
+    Theme.setThemeColor(color);
+  }
+}
